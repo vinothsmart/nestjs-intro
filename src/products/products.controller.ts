9713229557db203377c3c1fcbfc5bb8fd5a47a65
vinothsmart <s.vinothkanna90@gmail.com>
2021-07-02 13:05:42 +0530

    update title docu

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -27,5 +27,16 @@ export class ProductsController {
   @Get(':id')
   getProduct(@Param('id') prodId: string) {
     return this.productsService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -17,10 +17,31 @@ export class ProductsService {
   }
 
   getSingleProduct(productId: string) {
-    const product = this.products.find((prod) => prod.id == productId);
-    if (!product) {
-      return new NotFoundException('Could not find product');
-    }
+    const product = this.findProduct(productId)[0];
     return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.description = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id == id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find product');
+    }
+    return [product, productIndex];
   }
 }
