65ecc483e23a7be8cc74696dc4fd16d26f3c6247
vinothsmart <s.vinothkanna90@gmail.com>
2021-07-02 11:12:49 +0530

    service updated

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './products.module';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number) {
+    const newProduct = new Product(new Date().toString(), title, desc, price);
+    this.products.push(newProduct);
+  }
+}
