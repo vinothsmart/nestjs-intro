b1db0edf5dd85b2b51bd18b6600b022391b262be
vinothsmart <s.vinothkanna90@gmail.com>
2021-07-02 11:23:42 +0530

    contrl ad service upated

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,7 +6,9 @@ export class ProductsService {
   products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
-    const newProduct = new Product(new Date().toString(), title, desc, price);
+    const prodId = new Date().toString();
+    const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
+    return prodId;
   }
 }
